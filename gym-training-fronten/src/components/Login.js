826879da4009a34bd1340e2995de79c31b1af4cd
vcmxs <36706171+vcmxs@users.noreply.github.com>
2025-11-28 15:10:01 -0400

    Use environment variable for API URL in Login.js

--- gym-training-fronten/src/components/Login.js
+++ gym-training-fronten/src/components/Login.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/v1';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api/v1';
 
 function Login({ onLogin, onToggle }) {
   const [email, setEmail] = useState('');
