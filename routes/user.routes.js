041c28cd4f926969b7204155a5ade7a0a4b4ca7e
vcmxs <36706171+vcmxs@users.noreply.github.com>
2026-01-18 12:46:01 -0400

    Add files via upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,8 @@ const express = require('express');
 const userController = require('../controllers/user.controller');
 const upload = require('../config/upload');
 
+const { authenticateToken } = require('../middleware/auth');
+
 const router = express.Router();
 
 // User profile routes
@@ -10,7 +12,7 @@ router.get('/:userId', userController.getUserById);
 router.get('/:userId/profile', userController.getUserProfile);
 router.put('/:userId/profile', userController.updateUserProfile);
 router.put('/:userId/profile-picture', upload.single('profilePic'), userController.updateProfilePicture);
-router.delete('/:userId/account', userController.deleteOwnAccount);
+router.delete('/:userId/account', authenticateToken, userController.deleteOwnAccount);
 router.delete('/:userId/connection/:targetId', userController.removeConnection);
 
 module.exports = router;
