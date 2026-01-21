4081f938e0c2d43c6107205f21ff06a296b1412a
vcmxs <36706171+vcmxs@users.noreply.github.com>
2026-01-21 19:35:11 -0400

    Add files via upload

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const notificationController = require('../controllers/notification.controller');
-const auth = require('../middleware/auth');
+const { authenticateToken } = require('../middleware/auth');
 
 // All routes require authentication
-router.use(auth);
+router.use(authenticateToken);
 
 router.get('/', notificationController.getNotifications);
 router.patch('/read-all', notificationController.markAllAsRead);
