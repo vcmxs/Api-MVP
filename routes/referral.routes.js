69f26b7d691c622fef7801be9f5cb7e91d6071b7
vcmxs <36706171+vcmxs@users.noreply.github.com>
2026-01-27 17:06:51 -0400

    Add files via upload

---
+++ routes/referral.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const referralController = require('../controllers/referral.controller');
+const { authenticateToken } = require('../middleware/auth.middleware');
+
+// All routes here should be protected
+router.use(authenticateToken);
+
+router.get('/stats', referralController.getStats);
+router.get('/admin/stats', referralController.getAdminStats);
+
+module.exports = router;
