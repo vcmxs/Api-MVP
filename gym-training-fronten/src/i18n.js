d8f2ede5d754a1d8733375397c7f06a1eff4454c
vcmxs <36706171+vcmxs@users.noreply.github.com>
2026-02-10 10:17:58 -0400

    Add files via upload

--- gym-training-fronten/src/i18n.js
+++ gym-training-fronten/src/i18n.js
@@ -13,6 +13,7 @@ i18n
     .use(initReactI18next)
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
+    // Trigger Reload
     .init({
         resources: {
             en: {
