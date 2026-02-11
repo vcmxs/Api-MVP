5907f7ce7a7dba490fe6729b18cfef76b773248d
vcmxs <36706171+vcmxs@users.noreply.github.com>
2026-02-10 23:37:19 -0400

    Add files via upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 // routes/user.routes.js
 const express = require('express');
+const router = express.Router();
 const userController = require('../controllers/user.controller');
 const upload = require('../config/upload');
-
 const { authenticateToken } = require('../middleware/auth');
 
-const router = express.Router();
+// Weight History
+router.get('/:userId/weight-history', authenticateToken, userController.getWeightHistory);
 
 // User profile routes
 router.get('/:userId', userController.getUserById);
