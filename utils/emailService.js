993468d27ca006538e1c397ac44ee84a205e2268
vcmxs <36706171+vcmxs@users.noreply.github.com>
2026-02-28 14:16:19 -0400

    Add files via upload

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,14 +2,14 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use TLS
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
     tls: {
-        // do not fail on invalid certs in cloud environments (like Railway/Heroku)
         rejectUnauthorized: false
     }
 });
