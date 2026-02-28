2e4aa5bf284c25c25e566e7cd4c69a68f76a41c6
vcmxs <36706171+vcmxs@users.noreply.github.com>
2026-02-28 13:19:11 -0400

    Add files via upload

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,6 +9,15 @@ const router = express.Router();
 router.post('/register', upload.single('profilePic'), authController.register);
 
 // POST /api/v1/auth/login
-router.post('/login', authController.login);
+router.post('/login', authLimiter, authController.login);
+
+// POST /api/v1/auth/google
+router.post('/google-login', authLimiter, authController.googleLogin);
+
+// New Security and Verification endpoints
+router.post('/verify-email', authController.verifyEmail);
+router.post('/resend-pin', authLimiter, authController.resendPin);
+router.post('/forgot-password', authLimiter, authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
